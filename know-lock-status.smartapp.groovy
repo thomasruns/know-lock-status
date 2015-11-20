79b8d520b7ae842ec544dfc9ee64bd8c6897e88d
thomasruns <thomasruns@gmail.com>
2015-11-20 13:32:21 -0600

    Update license and function names.

--- know-lock-status.smartapp.groovy
+++ know-lock-status.smartapp.groovy
@@ -3,15 +3,23 @@
  *
  *  Copyright 2015 Thomas Vaughn
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
- *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 definition(
     name: "Know Lock Status",
@@ -32,8 +40,8 @@ preferences {
 }
 
 def installed() {
-    subscribe(lock1, "lock", checkTime)
-    subscribe(lock1, "unlock", checkTime)
+    subscribe(lock1, "lock", checkLock)
+    subscribe(lock1, "unlock", checkLock)
     
     initialize()
 }
@@ -44,11 +52,11 @@ def updated() {
 }
 
 def initialize() {
-    subscribe(lock1, "lock", checkTime)
-    subscribe(unlock1, "unlock", checkTime)
+    subscribe(lock1, "lock", checkLock)
+    subscribe(unlock1, "unlock", checkLock)
 }
 
-def checkTime(evt) {
+def checkLock(evt) {
     if(evt.value == "locked") {
         sendPush("$settings.lock1 Locked")
     }
